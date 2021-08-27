af885c74f7d7fe9f84c0fa11264e2adbf26c19ca
Matea Bach <matea.bach@gmail.com>
2021-08-27 17:26:31 -0400

    initial commit

---
+++ home.js
@@ -0,0 +1,13 @@
+let player = 0
+let computer = 0
+let winner = ''
+
+function computerPlay (playerSelection, computerSelection) {
+if (playerSelection === computerSelection)
+
+}
+const playerSelection= "rock";
+const computerSelection = computerPlay();
+console.log(computerplay(playerSelection,computerSelection));
+
+// function capitalize - make sure it can take strings that are lowecase, uppercase or both
